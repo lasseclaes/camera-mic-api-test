5d460d3121e457571c258741730d1ad05e5a6dc6
lasseclaes <lasseclaes@gmail.com>
2018-08-27 12:27:03 +0200

    camera issues resolved

--- js/app_v12.js
+++ js/app_v12.js
@@ -1,10 +1,15 @@
 //fra https://blog.prototypr.io/make-a-camera-web-app-tutorial-part-1-ec284af8dddf
 
 // Set constraints for the video stream
-//var constraints = { video: { facingMode: "user" }, audio: false };
-alert ("v4");
-var constraints = { audio: false, video: { facingMode: { exact: "environment" } } };
+alert ("v12");
+var userConstraints = { video: { facingMode: "user" }, audio: false };
+var rearConstraints = { audio: false, video: { facingMode: { exact: "environment" } } };
+
+var constraints;
+
+var selfie = true;
 //var constraints = { audio: false, facingMode: { exact: "environment" } };
+
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
@@ -12,6 +17,16 @@ const cameraView = document.querySelector("#camera--view"),
     cameraTrigger = document.querySelector("#camera--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
+    alert ("camera started");
+    if(selfie === true){
+      constraints = rearConstraints;
+      selfie = false;
+    }
+    else{
+      constraints = userConstraints;
+      selfie = true;
+    }
+
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
@@ -21,11 +36,11 @@ function cameraStart() {
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
-    console.log(navigator.mediaDevices
-        .getUserMedia(constraints));
+    //console.log(navigator.mediaDevices.getUserMedia(constraints));
 }
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
+    alert ("camera triggered");
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
@@ -36,3 +51,7 @@ cameraTrigger.onclick = function() {
 //window.addEventListener("load", cameraStart, false);
 //document.addEventListener()
 window.addEventListener("load", init, false);
+
+function init(){
+  document.getElementById('toggle-camera').addEventListener("click", function(){cameraStart()});
+}
